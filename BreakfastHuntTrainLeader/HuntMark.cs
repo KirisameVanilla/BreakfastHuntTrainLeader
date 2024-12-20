f71cde09ac77dd77b505c8670ca836cbc358b0d8
KirisameVanilla <vanillaaaajp@gmail.com>
2024-12-20 18:42:28 +0800

    small fix about relaying?

--- BreakfastHuntTrainLeader/HuntMark.cs
+++ BreakfastHuntTrainLeader/HuntMark.cs
@@ -36,23 +36,24 @@ public class HuntMark
 
     public void Relay()
     {
-        if (DService.ClientState.LocalPlayer == null) return;
-        FlagMark();
+        if (DService.ClientState.LocalPlayer == null || !FlagMark()) return;
         if (ExcelHelper.Worlds[DService.ClientState.LocalPlayer.CurrentWorld.Id].Name.RawString == Server)
             foreach (var command in Plugin.Config.RelayCommands)
-                ChatHelper.Instance.SendMessage(command + " " + Plugin.Config.同服扩散模板.Format(Instance));
+                Plugin.Tasks.Enqueue(() => 
+                                         ChatHelper.Instance.SendMessage(command + " " + Plugin.Config.同服扩散模板.Format(Instance)));
         else
             foreach (var command in Plugin.Config.RelayCommands)
-                ChatHelper.Instance.SendMessage(command + " " + Plugin.Config.跨服扩散模板.Format(Instance, Server));
-        
+                Plugin.Tasks.Enqueue(() => 
+                                         ChatHelper.Instance.SendMessage(command + " " + Plugin.Config.跨服扩散模板.Format(Instance, Server)));
     }
 
     public unsafe bool FlagMark()
     {
+        Plugin.Tasks.Abort();
         var agentMap = AgentMap.Instance();
         if (agentMap == null) return false;
         if (!agentMap->IsAgentActive() || agentMap->SelectedMapId != MapId)
-            agentMap->OpenMap(MapId, TerritoryId);
+            Plugin.Tasks.Enqueue(() => agentMap->OpenMap(MapId, TerritoryId));
         Plugin.Tasks.Enqueue(() => agentMap->IsAgentActive() && agentMap->SelectedMapId == MapId);
         Plugin.Tasks.DelayNext(100);
         Plugin.Tasks.Enqueue(() => agentMap->SetFlagMapMarker(TerritoryId, MapId, new Vector3(Position.X, 0f, Position.Y)));
